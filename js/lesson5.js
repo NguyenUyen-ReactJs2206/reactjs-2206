90ec89f5c2f07e7a144e95399cbf284846e979b5
Uyen <uyenntt17407@st.uel.edu.vn>
2022-07-21 23:54:29 +0700

    Full Lesson5-Js

--- js/lesson5.js
+++ js/lesson5.js
@@ -39,3 +39,61 @@ function number2() {
     }
     document.getElementById("snt").innerHTML = "Kết quả các số nguyên tố là:" + text;
 }
+
+//create order
+
+var listStudent = [
+    {name:"Nguyen Van D", score:10},
+    {name:"Nguyen Van A", score:2},
+    {name:"Nguyen Van V", score:5},
+    {name:"Nguyen Van F", score:8},
+    {name:"Nguyen Van O", score:3},
+    {name:"Nguyen Van M", score:6},
+    {name:"Nguyen Van T", score:7},
+];
+function showlistStudent() {
+    var bang = "";
+    for ( x in listStudent) {
+        bang +=
+        "<tr>" +
+        "<td>" +
+        listStudent[x]["name"] +
+        "</td>" +
+        "<td>" +
+        listStudent[x]["score"] +
+        "</td>" +
+        "</tr>";
+    }
+    document.getElementById("table_body").innerHTML = bang;
+  }
+  showlistStudent();
+
+/*Cach1: function searchName() {
+  listStudent.sort(function (a, b) {
+    if (a.name.toLocaleLowerCase() < b.name.toLocaleLowerCase()) {
+      return -1;
+    } else if (a.name.toLocaleLowerCase() > b.name.toLocaleLowerCase()) {
+      return 1;
+    } else return 0;
+  });
+  showlistStudent();
+}*/
+//Cách2:
+function searchName() {
+  listStudent.sort(function(a, b){
+    let x = a.name.toLowerCase();
+    let y = b.name.toLowerCase();
+    if (x < y) {return -1;}
+    if (x > y) {return 1;}
+    return 0;
+  });
+  showlistStudent();
+}
+
+
+function searchScore() {
+  listStudent.sort(function (a, b) {
+    return a.score - b.score;
+  });
+  showlistStudent();
+}
