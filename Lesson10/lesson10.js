0aa862d8591c09283558ca9d6297710700698d27
Uyen <uyenntt17407@st.uel.edu.vn>
2022-08-14 12:01:42 +0700

    Lesson13

--- Lesson10/lesson10.js
+++ Lesson10/lesson10.js
@@ -39,9 +39,9 @@ $(function () {
 
     function showPaginate(currentPage) {
         //Lấy ra tất cả sản phẩm
-        let numberPerPage = 16;   //tong so sp
+        let numberPerPage = 4;   //tong so sp
         let products = $('.product');
-        let totalProduct = products.lenght;   // tong sp
+        let totalProduct = products.length;   // tong sp
 
         let from = (currentPage - 1) * numberPerPage;   //
         let to = currentPage * numberPerPage - 1;
@@ -58,6 +58,7 @@ $(function () {
 
         let paginateHTML = '';
         let pageCount = Math.ceil(totalProduct / numberPerPage);
+        
 
         for (let i = 1; i <= pageCount; i++) {
 
@@ -66,45 +67,90 @@ $(function () {
         <span class="page-link">${i}</span>
       </li>`;
             } else {
-                paginateHTML += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`;
+                paginateHTML += `<li class="page-item"><a class="page-link" >${i}</a></li>`;
             };
         };
 
         $('#paginate').empty().append(paginateHTML);
+        
     };
 
 
 
     //DEMO SAP XEP
     //B1: Bat su kien onchange
-    $('#orderProduct').change(function () {
-        //B2: Lay kieu sap xep tang dan hoac giam dan
+
+    //B2: Lay kieu sap xep tang dan hoac giam dan
+    $('body').on('change', '#sortProduct', function () {
+        let sortType = $(this).val();
+
+        sortProduct(sortType);
+    });
 
 
+    function sortProduct(sortType) {
         //B3: Lay duoc gia tri cua san pham sau khi giam
         let listPrice = [];
         $('.product').each(function () {
-            let price = parseIn($(this).find('i:not(.sale-off)').text().replace('$', ''));
+            let price = $(this).find('i:not(.sale-off)').text().replace('$', '');
             //gán price = tìm thẻ i không có class là sale-off trong tất cả sản phẩm, sau đó lấy ra giá trị của nó và thay thế $ thành rỗng và cuối cùng là chuyển string thành number
 
             //B5: Mỗi sp thêm 1 class mới
-            $('.product').addClass(price);
-            listPrice.push(price);
-        })
+            $(this).addClass(price); // class is String
+
+
+            listPrice.push(parseInt(price)); // đổi string thành number
+        });
+
 
         //B4: sắp xếp mảng theo các giá trị đã lấy được
-        let priceOrdered = [];
-        listPrice.sort(function (a, b) { return a - b });
-        priceOrdered.push(listPrice);
+        if (sortType == 'decrease') {
+            listPrice.sort(function (a, b) { return b - a });
+        } else {
+            listPrice.sort(function (a, b) { return a - b });
+        }
 
         //B5: Mỗi sp thêm 1 class mới
         // $('.product').addClass(price);
 
-        //B6: Duyệt mảng đã được sắp xếp và hiển thị sản phẩm tương ứng với giá
-        priceOrdered.forEach(function (index, price) { //tìm các sp có class là price và hiển thị lên
-            $('.' + price).show();
+        let productListSorted = [];
 
+        //B6: Duyệt mảng đã được sắp xếp và hiển thị sản phẩm tương ứng với giá
+        listPrice.forEach(function (price, index) { //tìm các sp có class là price và hiển thị lên
+            productListSorted.push($('#products').find('.' + price));// not show -
         });
 
-    });
+        $('.product').remove();
+        $('#products').prepend(productListSorted);
+
+        showPaginate(1);
+    };
+
+showCartNumber();
+function showCartNumber() {
+    let currentCartNumber = localStorage.getItem('cart-number');
+    $('.cart-number').text(currentCartNumbber);
+}
+
+$('body').on('click', '.buy', function (e) {
+    e.preventDefault();
+
+    let currentCartNumber = +$('.cart-number').text();
+    let newCartNumber = currentCartNumber + 1;
+
+    $('.cart-number').text(newCartNumber);
+
+    localStorage.setItem('cart-number', newCartNumber);
+
+    showNsgSuccess();
 });
+
+function showNsgSuccess() {
+    var x = $('#snackbar');
+    x.className = 'show';
+    setTimeout(function () {
+        x.className = x.className.replace('show','');
+    },3000);
+}
+});
+
